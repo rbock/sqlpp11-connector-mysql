81e460abff5af4be873d76d0f379324ac4146053
rbock <rbock@eudoxos.de>
2016-08-31 09:56:22 +0200

    Next attempt

--- cmake/FindMySql.cmake
+++ cmake/FindMySql.cmake
@@ -18,6 +18,8 @@ if (NOT DEFINED MSVC)
 else()
 	file(GLOB MYSQL_GLOB_INCLUDE "$ENV{SystemDrive}/Program Files/mariadb-connector-c/lib/mariadb/include")
 	file(GLOB MYSQL_GLOB_INCLUDE86 "$ENV{SystemDrive}/Program Files (x86)/mariadb-connector-c/lib/mariadb/include")
+	MESSAGE(MYSQL_GLOB_INCLUDE)
+	MESSAGE(MYSQL_GLOB_INCLUDE86)
 	find_path(MYSQL_INCLUDE_DIR
 		NAMES mysql.h
 		PATH_SUFFIXES include
@@ -27,8 +29,10 @@ else()
 
 	file(GLOB MYSQL_GLOB_LIB "$ENV{SystemDrive}/Program Files/mariadb-connector-c/lib/mariadb/lib")
 	file(GLOB MYSQL_GLOB_LIB86 "$ENV{SystemDrive}/Program Files (x86)/mariadb-connector-c/lib/mariadb/lib")
+	MESSAGE(MYSQL_GLOB_LIB)
+	MESSAGE(MYSQL_GLOB_LIB86)
 	find_library(MYSQL_LIBRARY
-		NAMES mariadb-connector-c/lib/mariadb/libmariadb.dll
+		NAMES libmariadb
 		PATH_SUFFIXES lib
 		PATHS ${MYSQL_GLOB_LIB}
 					${MYSQL_GLOB_LIB86}
