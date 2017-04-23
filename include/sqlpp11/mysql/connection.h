9231a782329787da7eefc2e8ed8030697ca17b22
Frank Park <fpark12@icloud.com>
2017-04-23 16:31:31 -0400

    add connection_config typedef in connection

--- include/sqlpp11/mysql/connection.h
+++ include/sqlpp11/mysql/connection.h
@@ -70,6 +70,7 @@ namespace sqlpp
       std::stringstream _os;
     };
 
+    struct connection_config;
     class connection : public sqlpp::connection
     {
       std::unique_ptr<detail::connection_handle_t> _handle;
@@ -89,6 +90,7 @@ namespace sqlpp
       size_t run_prepared_remove_impl(prepared_statement_t& prepared_statement);
 
     public:
+      typedef ::sqlpp::mysql::connection_config connection_config;
       using _prepared_statement_t = ::sqlpp::mysql::prepared_statement_t;
       using _context_t = serializer_t;
       using _serializer_context_t = _context_t;
